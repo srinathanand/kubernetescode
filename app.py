2032692545037276b7d40a483920e9ce32aa7eba
Srinath Anand <srinath.anand@gmail.com>
2022-01-10 14:12:09 -0500

    testing github webhook

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     #return 'Please subscribe, like, and comment on this video, TY!!!'
-    return 'Hello Docker!'
+    return 'Testing Github webhook!'
